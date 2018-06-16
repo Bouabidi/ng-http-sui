a8ce6149dc9d6125bf1ac8a3e7aed032d13aee4a
Michael Wanyoike <micwanyoike@gmail.com>
2018-06-16 12:37:40 +0300

    Implemented post-form component logic

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../../models/post.model';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../../services/post.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-post-form',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostFormComponent implements OnInit {
 
-  constructor() { }
+  @Input() post:Post;
+  create:boolean = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getPost();
   }
 
+  getPost() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    if(id) {
+      this.postService.getPost(id)
+        .subscribe(post => this.post = post);
+    } else {
+      this.create = true;
+      this.post = new Post();
+    }
+  }
+
+  save() {
+    if(this.create) {
+      this.postService.addPost(this.post)
+        .subscribe(() => this.goBack());
+    } else {
+      this.postService.updatePost(this.post)
+        .subscribe(() => this.goBack());
+    }
+  }
+
+  goBack() {
+    this.location.back();
+  }
 }
