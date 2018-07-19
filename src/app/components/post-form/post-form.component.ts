d196aea6951d2c2a20e79774edf33d849fe1019e
Michael Wanyoike <micwanyoike@gmail.com>
2018-07-19 15:49:54 +0300

    Implemented view post

--- src/app/components/post-form/post-form.component.ts
+++ src/app/components/post-form/post-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from '../../models/post.model';
 import { ActivatedRoute } from '@angular/router';
 import { PostService } from '../../services/post.service';
--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../../models/post.model';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from '../../services/post.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-post',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { }
+  post: Post;
+
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getPost();
+  }
+
+  getPost() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    if (id) {
+      console.log(`Found post ${id}`);
+      this.postService.getPost(id)
+        .subscribe(post => this.post = post)
+    } else {
+      console.log('Selected post not found');
+      this.location.back();
+    }
   }
 
 }
